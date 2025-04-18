9a6929ea9ac800c7de9ab88091d40c7cf4235491
Andrew Gilbrough <andrew@gilbrough.com>
2025-04-17 21:42:33 -0700

    Removed ConversionError

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@ pub mod asyncs;
 pub mod time;
 extern crate alloc;
 
+/*
 /// Basic `ConversionError` for when primitives can't be converted to/from bytes because of invalid
 /// states. Most modules use their own errors for when there is more information to report.
 #[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
 struct ConversionError(pub ());
+*/
